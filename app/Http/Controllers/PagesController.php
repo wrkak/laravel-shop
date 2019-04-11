c472de55559abb12edf742dc061fbd69b075a2d4
k <710312372@qq.com>
2019-04-11 08:47:15 +0000

    邮箱验证中间件

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,4 +10,10 @@ class PagesController extends Controller
     {
         return view('pages.root');
     }
+
+    public function emailVerifyNotice(Request $request)
+    {
+        return view('pages.email_verify_notice');
+    }
+
 }
