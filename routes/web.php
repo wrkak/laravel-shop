645886a7e530ae5729bdb2c82228eea85fe29288
k <710312372@qq.com>
2019-04-11 10:56:06 +0000

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,18 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function() {
-    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
-  	Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+     Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+  	 Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
 
 
 
 
     // 需要验证邮箱
     Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+         Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+         Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+         Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
     });
     
 });
