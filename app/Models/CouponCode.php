2da54caef1ef05b34aecc2482d566a2fcf7101bb
k <710312372@qq.com>
2019-04-18 03:31:04 +0000

    修复优惠券逻辑 Bug  增加支付宝回调状态校验  同意退款时清空拒绝退款理由 fix timezone

--- app/Models/CouponCode.php
+++ app/Models/CouponCode.php
@@ -94,7 +94,7 @@ class CouponCode extends Model
                         ->where('closed', false);
                 })->orWhere(function($query) {
                     $query->whereNotNull('paid_at')
-                        ->where('refund_status', Order::REFUND_STATUS_PENDING);
+                        ->where('refund_status','!=', Order::REFUND_STATUS_SUCCESS);
                 });
             })
             ->exists();
